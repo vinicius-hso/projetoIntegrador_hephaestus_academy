7025aa3578fa0de9a749da85dffb2d275d93a358
Debora Faria <deeborafaria@gmail.com>
2020-11-24 12:52:38 -0300

    Add files via upload

---
+++ codigos/hephaestus_academy/sobre_nos.php
@@ -0,0 +1,38 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-green layout-top-nav">
+<div>
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+
+
+	<!-- Main content -->
+	<main role="main">
+	    
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="height-adjust">
+
+		<!-- Aqui começa o texto Sobre Nós -->
+		<h1><b>Sobre Nós</b><hr /></h1>
+
+		<div>
+
+		<p style="text-indent: 30px;"><big>Somos a Hephaestus Academy, uma plataforma onde você encontra os melhores conteúdos didáticos na área de Tecnologia da Informação para alavancar seus estudos e aprimorar seus conhecimentos.</big></p>
+		<p style="text-indent: 30px;"><big>Nossa plataforma foi idealizada para atender a demanda de conteúdos didáticos com qualidade e  praticidade através de um preço acessível. Você irá encontrar na Hephaestus Academy conteúdos produzidos pelos melhores profissionais na área de Arquitetura e Organização de Computadores, Laboratório de Hardware, Língua Inglesa, Língua Portuguesa, Lógica de Programação e Matemática Discreta. Além disso, nossa plataforma está em constante  expansão, e com cada vez mais variedade de conteúdos que irão elevar seu conhecimento e habilidades para o mercado da tecnologia da informação.</big></p>
+		<p style="text-indent: 30px;"><big>Nossa plataforma é simples e intuitiva. Para acessar basta se <a href="signup.php"><b>  cadastrar</b></a>, escolher um conteúdo didático de sua preferência no catálogo, adicionar ao carrinho, realizar o pagamento e efetuar o download em seu pc.</big></p>
+		<p style="text-indent: 30px;"><big>Uma forma divertida e intuitiva de aprender. Capaz de atender usuários de diversas faixas etárias e diferentes necessidades, proporcionando conhecimento de forma clara e por um preço acessível.</big></p></br>
+		<p><big>Ficou alguma dúvida?
+		<a href="contato.php"> Entre em contato conosco!</big></a>
+		</p><br>
+        
+	    </div>
+
+	</main>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
