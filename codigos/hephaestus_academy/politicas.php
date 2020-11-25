c32a42fb4176692a574262de8d41f915695b1021
Vinicius Oliveira <67213864+vinicius-hso@users.noreply.github.com>
2020-11-25 07:51:36 -0300

    Add páginas Políticas e Termos

---
+++ codigos/hephaestus_academy/politicas.php
@@ -0,0 +1,36 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-green layout-top-nav">
+<div>
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+
+
+	<!-- Main content -->
+	<main role="main">
+	    
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="height-adjust">
+
+		<!-- Aqui começa o texto Sobre Nós -->
+		<div class="box-politicas">
+
+		<h1><b>Políticas de Privacidade</b></h1>
+			<hr>
+
+			<p style="text-indent: 30px;"><big>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis facilis saepe ipsam, accusantium sed unde aut tempora? Nostrum perferendis asperiores pariatur architecto labore consequatur iure, libero harum voluptates tempora voluptatibus!</big></p>
+			<p style="text-indent: 30px;"><big>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, error! Reiciendis temporibus nobis placeat, molestiae architecto ipsum doloremque cumque velit eveniet sed quae inventore, laborum veniam at mollitia! Maiores, recusandae? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati voluptas autem neque eligendi ipsa. Vel debitis odit laborum impedit, repellat possimus aspernatur eaque dignissimos ab id, sequi vitae fuga minus?</big></p>
+			<p style="text-indent: 30px;"><big>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, nisi quia reiciendis amet veritatis ex eligendi quidem inventore assumenda facere, officia sequi, dolorem enim illo doloribus similique eveniet quos quae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ipsum, sunt tempora molestias dolores veniam dignissimos rerum earum omnis eligendi iure ipsam qui totam, suscipit laborum debitis assumenda quae? In.</big></p>
+			<p style="text-indent: 30px;"><big>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat maiores accusamus quasi nisi, necessitatibus, aut suscipit, possimus facilis asperiores quod voluptate minima libero soluta eveniet hic assumenda placeat? Excepturi!</big></p></br>
+        
+	    </div>
+
+	</main>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
