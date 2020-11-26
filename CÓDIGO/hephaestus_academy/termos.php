65fcb2ae7e2d069a590e08de6f33dc75b4e4722a
Vinicius Oliveira <67213864+vinicius-hso@users.noreply.github.com>
2020-11-25 21:40:34 -0300

    Add page Termos

---
+++ CÓDIGO/hephaestus_academy/termos.php
@@ -0,0 +1,34 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-green layout-top-nav">
+<div>
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+
+
+	<!-- Main content -->
+	<main role="main">
+	    
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="height-adjust">
+
+		<!-- Aqui começa o texto Sobre Nós -->
+		<div class="box-termos">
+
+			<h1><b>Termos de Uso</b></h1>
+			<hr>
+
+			<p style="text-indent: 30px;"><big>Os conteúdos vendidos neste site, seja texto, vídeo ou jogo, é propriedade da Hephaestus Academy e daqueles que os produziram. Ao ser efetuada, a compra deste conteúdo permite ao usuário o uso dele somente para fins didáticos. A redistribuição sem autorização desse material é proibida por lei, como dito no art. 184 do código penal.</big></p>
+			<br>
+			<p style="text-indent: 30px;"><big>“O art. 184 do Código Penal, o Decreto nº 5.244, de 2004 e as Leis no 9.609 e 9.610/98), pode-se dizer que a legislação no Brasil define a pirataria como o crime consistente em qualquer violação de direito de autor ou direitos conexos para obras intelectuais ou softwares, e prevê penas que variam de 3 meses a 4 anos, com ou sem multa.”</big></p>
+	    </div>
+
+	</main>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
