4bdc101d76ce084bf70032ec91f99c840763b320
Vinicius Oliveira <67213864+vinicius-hso@users.noreply.github.com>
2020-11-25 21:41:07 -0300

    Add page Politicas

---
+++ CÓDIGO/hephaestus_academy/politicas.php
@@ -0,0 +1,38 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-green layout-top-nav">
+<div>
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+
+
+	<!-- Main content -->
+	<main role="main">
+	    
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="height-adjust">
+
+		<!-- Aqui começa o texto Sobre Nós -->
+		<div class="box-politicas">
+
+			<h1><b>Políticas de Privacidade</b></h1>
+			<hr>
+
+			<p style="text-indent: 30px;"><big>A Hephaestus Academy respeita a privacidade individual e valoriza a confiança de seus usuários, clientes e parceiros. Por esta razão, estamos dispostos a proteger a informação pessoal que venha a compartilhar conosco. O texto a seguir descreve como utilizamos essa informação. </big></p>
+			<h2><u>Recepção de dados</u></h2>
+			<p style="text-indent: 30px;"><big>Ao nos ceder suas informações pessoais, elas serão acessadas apenas pelos nossos serviços. É considerado informação pessoal tudo que pode ser utilizado para identificar um usuário, como, por exemplo, o nome, endereço eletrônico, idade, gênero, profissão e os demais dados que podemos necessitar para oferecer nossos serviços ou até para melhorar o funcionamento do site. Nós não registramos automaticamente as suas informações pessoais, incluindo o seu endereço de IP. O registro dos endereços IP daqueles que utilizam o nosso site servem apenas para a análise estatística de tráfego. Os dados fornecidos não serão fornecidos para terceiros sem o seu consentimento, mas podem ser utilizados dentro do site para fins de processamento. </big></p>
+			<br>
+			<h2><u>Solicitação e Atualização de Dados</u></h2>
+			<p style="text-indent: 30px;"><big>Caso deseje ter acesso às suas informações, apagá-las dos nossos registros ou modificá-las, entre em contato conosco para realizar a sua requisição. A Hephaestus Academy irá prontamente atender seus direitos e tomar as devidas providências para que a sua questão seja avaliada e respondida o mais rápido possível.</big></p>
+        
+	    </div>
+
+	</main>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
