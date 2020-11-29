eff0c4fc34f620ba701599b42f251e4ef1464f52
Vinicius Oliveira <67213864+vinicius-hso@users.noreply.github.com>
2020-11-28 23:57:50 -0300

    Add página Promocao

---
+++ CÓDIGO/hephaestus_academy/promocao.php
@@ -0,0 +1,39 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-green layout-top-nav">
+<div>
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+
+
+	<!-- Main content -->
+	<main role="main">
+	    
+	    <!-- Main jumbotron for a primary marketing message or call to action -->
+	    <div class="height-adjust">
+
+		 <!-- Aqui começa o conteúdo -->
+			<div class="box-div-promocao">
+
+				<h1>PROMOÇÃO!</h1>
+				<h3>Game Quiz Hephaestus Academy</h3>
+				<hr>
+				<p> A Hephaestus Academy está com uma super promoção para você!</p>
+				<p>Na compra de qualquer conteúdo de texto ou vídeo você ganha um Game Quiz interativo com questões  referentes ao tema do conteúdo adquirido para praticar seus conhecimentos e auxiliar seus estudos. Assim fica muito mais divertido aprender!</p>
+				<p>O Game Quiz da Hephaestus Academy foi desenvolvido em Python, e você pode jogar rodando o código através do Shell se você tiver o Python e as bibliotecas necessárias instaladas. Mas se você não tiver, não tem problema! Você pode executar a versão aplicativo, você receberá as duas versões junto com o download do seu conteúdo.</p>
+				<p>O Quiz contém de 10 a 30 questões variando de acordo com o conteúdo e você deverá responder em um determinado limite de tempo, mas cuidado, pois um único erro acaba com o jogo. Ao final você conseguirá visualizar seu número de acertos e avaliar seu grau de aproveitamento do conteúdo estudado. Legal, não é? A Hephaestus Academy tem para você as melhores formas de aprendizado, sempre com qualidade e praticidade.</p>
+				<p>Não perca tempo! <a href="all_category.php">Confira nossos conteúdos</a> e adquira o seu e leve de brinde um super <b>Game Quiz Hephaestus Academy</b></p>
+
+			
+			</div>
+		</div>
+
+	</main>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
